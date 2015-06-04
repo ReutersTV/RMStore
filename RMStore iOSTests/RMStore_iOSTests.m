d09f5de9a4ef4aaa4271e21aa8b7e96119b78fbf
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2015-06-04 15:15:49 +0100

    Added RMStore dynamic framework.

---
+++ RMStore iOSTests/RMStore_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  RMStore_iOSTests.m
+//  RMStore iOSTests
+//
+//  Created by Martin Lloyd on 04/06/2015.
+//  Copyright (c) 2015 Robot Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RMStore_iOSTests : XCTestCase
+
+@end
+
+@implementation RMStore_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
